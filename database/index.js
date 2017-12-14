350c85fb21959e438aaecf545269b88bd14e3bdf
Carter Chavez <Carter.chavez17@gmail.com>
2017-12-14 14:03:48 -0800

    connect DB connection

---
+++ database/index.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('testdb', '', '', {
+  host: 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 9,
+    min: 0,
+    idle: 10000
+  }
+});
+
+
+
+
+
+
+sequelize.authenticate().then(() => {
+  console.log("Success!");
+}).catch((err) => {
+  console.log(err);
+});
